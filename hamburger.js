d623c0746dec3b07157ca8b4ed6245423f4889ca
wesleyjv <40523031+wesleyjv@users.noreply.github.com>
2024-10-27 22:30:09 +0100

    update in files

--- hamburger.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-// Toggle the navigation links when hamburger is clicked
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active'); // Toggle the 'active' class
-});
